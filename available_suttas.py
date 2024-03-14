6a671c41c0008667ec40449ffdb26679b2234225
Ishaan Varshney <ishaan.varshney@gmail.com>
2024-03-14 16:35:48 +0100

    Update available_suttas.py (#43)
    * Update available_suttas.py
    
    * Update available suttas list
    
    * Update available_suttas.py
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>
    Co-authored-by: Bhikkhu Anīgha <53958588+bkhpanigha@users.noreply.github.com>

--- available_suttas.py
+++ available_suttas.py
@@ -26,6 +26,8 @@ available_suttas.sort(key=lambda x: int(x['id'][2:]))
 # Create a JSON object with the array of available suttas
 output = {'available_suttas': available_suttas}
 
-# Write the JSON object to a file
-with open('available_suttas.json', 'w', encoding='utf-8') as out_file:
-    json.dump(output, out_file, ensure_ascii=False, indent=4)
+
+# Write the JSON object to a file if it's not empty
+if len(available_suttas) > 0:
+    with open('available_suttas.json', 'w', encoding='utf-8') as out_file:
+        json.dump(output, out_file, ensure_ascii=False, indent=4)
