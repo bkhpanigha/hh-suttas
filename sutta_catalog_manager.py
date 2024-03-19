97b53386b3ece541401abdd7b9faa660a3b4cbef
Ishaan Varshney <ishaan.varshney@gmail.com>
2024-03-19 21:08:43 +0100

    update gh actions for files_to_cache updates

--- sutta_catalog_manager.py
+++ sutta_catalog_manager.py
@@ -3,6 +3,7 @@ import re
 from pathlib import Path
 import os
 
+
 def load_available_suttas(suttas_dir_path):
     suttas_dir = Path(suttas_dir_path)
     available_suttas = []
@@ -16,15 +17,13 @@ def load_available_suttas(suttas_dir_path):
                 key = f"mn{match.group(1)}:0.2"
                 sutta_title = data.get(key)
                 if sutta_title:
-                    available_suttas.append({"id": f"MN {match.group(1)}", "title": sutta_title})
+                    available_suttas.append(
+                        {"id": f"MN {match.group(1)}", "title": sutta_title}
+                    )
 
     available_suttas.sort(key=lambda x: int(x["id"][2:]))
     return available_suttas
 
-def write_available_suttas_to_file(available_suttas, output_file):
-    if available_suttas:
-        with open(output_file, "w", encoding="utf-8") as out_file:
-            json.dump({"available_suttas": available_suttas}, out_file, ensure_ascii=False, indent=4)
 
 def generate_corresponding_files_list(available_suttas, output_file):
     files_to_cache = []
@@ -37,19 +36,29 @@ def generate_corresponding_files_list(available_suttas, output_file):
 
     for sutta in available_suttas:
         formatted_sutta_id = sutta["id"].split()[1].lower()
-        files_to_cache.extend([
-            f"suttas/html/mn/mn{formatted_sutta_id}_html.json",
-            f"suttas/comment/mn/mn{formatted_sutta_id}_comment.json",
-            f"suttas/root/mn/mn{formatted_sutta_id}_root-pli-ms.json",
-            f"suttas/translation_en/mn/mn{formatted_sutta_id}.json",
-        ])
+        files_to_cache.extend(
+            [
+                f"suttas/html/mn/mn{formatted_sutta_id}_html.json",
+                f"suttas/comment/mn/mn{formatted_sutta_id}_comment.json",
+                f"suttas/root/mn/mn{formatted_sutta_id}_root-pli-ms.json",
+                f"suttas/translation_en/mn/mn{formatted_sutta_id}.json",
+            ]
+        )
 
     with open(output_file, "w", encoding="utf-8") as out_file:
         json.dump(files_to_cache, out_file, ensure_ascii=False, indent=4)
+
 
 # Main execution
 if __name__ == "__main__":
     suttas_dir_path = "suttas/translation_en/mn"
     available_suttas = load_available_suttas(suttas_dir_path)
-    write_available_suttas_to_file(available_suttas, "available_suttas.json")
-    generate_corresponding_files_list(available_suttas, "files_to_cache.json")
+    if available_suttas:
+        with open("available_suttas.json", "w", encoding="utf-8") as out_file:
+            json.dump(
+                {"available_suttas": available_suttas},
+                out_file,
+                ensure_ascii=False,
+                indent=4,
+            )
+        generate_corresponding_files_list(available_suttas, "files_to_cache.json")
