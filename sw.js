62e6a63b520378fe90f3bda0fab3bdb8199c45e7
Ishaan Varshney <ishaan.varshney@gmail.com>
2024-03-19 21:00:52 +0100

    update sw.js to use cached files and add some better logging

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+const version = 'v3'; // Update this version manually when filesToCache changes
+
 // static files to cache
 const filesToCache = [
   "roboto-lightest.woff",
@@ -7,27 +9,34 @@ const filesToCache = [
   "index.html",
   "index.js",
 ];
+// Load additional files from files_to_cache.json and add them to filesToCache
+fetch('files_to_cache.json')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(file => {
+      filesToCache.push(file);
+    });
+  })
+  .catch(error => console.error('Error loading files_to_cache.json:', error));
 
 
 
-const cacheName = 'hh-suttas-cache-v2';
+const cacheName = `hh-suttas-cache-${version}`;
 
 self.addEventListener('install', event => {
-
-  // event.waitUntil(
-  //   caches.open(cacheName)
-  //     .then(cache => cache.addAll(filesToCache))
-  //     .then(() => self.skipWaiting()) // Activate the new service worker immediately
-  // );
+  self.skipWaiting()
 });
 
 self.addEventListener('message', event => {
-  // Check if the message is to trigger caching
   if (event.data && event.data.action === 'cacheResources') {
-    // Perform caching operations here
     caches.open(cacheName)
       .then(cache => {
-        return cache.addAll(filesToCache);
+        const addPromises = filesToCache.map(file => {
+          return cache.add(file).catch(() => {
+            console.error('Caching failed for resource:', file);
+          });
+        });
+        return Promise.all(addPromises);
       })
       .then(() => {
         self.clients.matchAll().then(clients => {
@@ -37,45 +46,44 @@ self.addEventListener('message', event => {
         });
       })
       .catch(error => {
-        client.postMessage({ action: 'cachingError' });
-        console.error(error)
+        console.error('Unexpected error:', error);
       });
   }
 });
 
-self.addEventListener('fetch', function(event) {
-  if(!event.request.url.startsWith('http')){
+self.addEventListener('fetch', function (event) {
+  if (!event.request.url.startsWith('http')) {
     return
- }
+  }
   event.respondWith(
     // Fetch from network first
     fetch(event.request)
-      .then(function(response) {
+      .then(function (response) {
         // If successful response, clone it, cache it, and return
-        
+
         if (response && response.status === 200) {
           var responseToCache = response.clone();
-          caches.open(cacheName).then(function(cache) {
+          caches.open(cacheName).then(function (cache) {
             cache.put(event.request, responseToCache);
           });
         }
         return response;
       })
-      .catch(function() {
+      .catch(function () {
         // If fetch fails, try to get the response from cache
         console.log("index.html:")
         return caches.match(event.request)
-          .then(function(response) {
+          .then(function (response) {
             // If found in cache, return response
             if (response) {
-            
+
               return response;
             }
             // If not found in cache, respond with a basic offline page
-            
+
             return caches.match('/index.html');
           });
-        
+
       })
   );
 });
