d6cd5d4fb47bf914f930bf0c7c7ed77d64ba2399
Ishaan Varshney <ishaan.varshney@gmail.com>
2024-03-21 05:20:54 +0100

    add transparent icon button (#56)

--- index.js
+++ index.js
@@ -68,20 +68,30 @@ function displaySuttas(suttas) {
     }
   });
 
-  infoButton.addEventListener("click", function() {
-    
+  infoButton.addEventListener("click", function (event) {
+    event.stopPropagation(); // Prevent click from immediately propagating to document
     let notificationBox = document.querySelector('.info-notification-box')
     if (!notificationBox) {
       notificationBox = document.createElement('div');
       notificationBox.classList.add('info-notification-box');
       document.body.appendChild(notificationBox);
     }
-    
-    if (notificationBox.style.display == 'block'){
+
+    if (notificationBox.style.display == 'block') {
       notificationBox.style.display = 'none';
     } else {
       notificationBox.textContent = "The ‘Download’ button makes the site available offline on the current web browser at the same URL (suttas.hillsidehermitage.org).\n\nThe site can also be installed as an application on mobile phones, by tapping ‘Install’ at the menu on the top right corner. Note that hitting the download button is still necessary to make it available offline through the app.\n\nIf downloading again (e.g., when new Suttas become available), make sure to first clear the site's data on your browser/app and reload the page.";
       notificationBox.style.display = 'block';
+    }
+  });
+  // Add event listener to document to hide notificationBox when clicking outside
+  document.addEventListener("click", function (event) {
+    let notificationBox = document.querySelector('.info-notification-box');
+    if (notificationBox && notificationBox.style.display == 'block') {
+      // Check if the click is outside the notificationBox and not on the infoButton
+      if (!notificationBox.contains(event.target) && event.target !== infoButton) {
+        notificationBox.style.display = 'none';
+      }
     }
   });
 
