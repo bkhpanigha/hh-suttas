5e84dbfa018722e980715f43d1f6c90574cf582f
gachi <59866185+Reptilioo@users.noreply.github.com>
2024-05-23 14:31:33 +0200

    Update index.js
    - Fixed issue where when a user was typing in the search box, the sutta search result list was appended at the end of the displayed list, instead of replacing it.
    
    - Added display of authors and headings when doing a search

--- index.js
+++ index.js
@@ -44,10 +44,10 @@ async function getAvailableSuttas({ mergedTitle = true } = {}) {
 
 function searchSuttas(pattern) {
   if (!fuse) { pattern = "" }; // if Fuse isn't initialized, return empty array
-  pattern = pattern.replace(/([a-zA-Z]{2})(\d+)/, '$1 $2');
+  //pattern = pattern.replace(/([a-zA-Z]{2})(\d+)/, '$1 $2');
   let results = fuse.search(pattern).map(result => result.item);
   // join up the id with the titles to be displayed
-  return results.map(sutta => `${sutta.id}: ${sutta.title.trim()}`);
+  return results.map(sutta => `${sutta.id}: ${sutta.title.trim()}${sutta.author ? `: ${sutta.author}` : ':'}${sutta.heading ? `: ${sutta.heading}` : ':'}`);
 }
 
 function getSuttaTitleById(id) {
@@ -81,8 +81,7 @@ function displaySuttas(suttas) {
     "kn": "Khuddaka Nikāya"
   };
   let currentGroup = -1;
-  suttaArea.innerHTML += `<ul style="margin-top: 20px;">${suttas.map(sutta => {
-
+  suttaArea.innerHTML = `<ul style="margin-top: 20px;">${suttas.map(sutta => {
 
     const parts = sutta.split(':');
     const id = parts[0].trim().replace(/\s+/g, '');
