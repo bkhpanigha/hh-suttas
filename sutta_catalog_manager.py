914e5c0af0db8101c150b06627f4bf6cae98ee40
gachi <59866185+Reptilioo@users.noreply.github.com>
2024-06-16 21:33:14 +0200

    Update sutta_catalog_manager.py
    Add a condition to check if a file has a name and extension in order to be added to files_to_cache so as to set aside unneeded file like system files

--- sutta_catalog_manager.py
+++ sutta_catalog_manager.py
@@ -182,7 +182,9 @@ def generate_corresponding_files_list(available_suttas, output_file):
             for file in os.listdir(directory):
                 file_path = os.path.join(directory, file)
                 if os.path.isfile(file_path) and 'git' not in file_path:
-                    files_to_cache.append(os.path.relpath(file_path, '.'))
+                    # Check if the file has a name and an extension to not cache unneeded system files
+                    if '.' in file and file.rsplit('.', 1)[0] and file.rsplit('.', 1)[1]:
+                        files_to_cache.append(os.path.relpath(file_path, '.'))
         else:
             for root, _, files in os.walk(directory):
                 if 'git' not in root:
