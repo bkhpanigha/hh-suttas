01d7be164355aee08da537f5c01eac270c3bb73f
Ishaan Varshney <ishaan.varshney@gmail.com>
2024-06-25 22:19:31 +0200

    add bookmarks upload test

--- index.test.js
+++ index.test.js
@@ -1,7 +1,22 @@
 const puppeteer = require('puppeteer');
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 
+// Retry logic
+const retry = async (fn, retries = 3, delay = 1000) => {
+  for (let i = 0; i < retries; i++) {
+    try {
+      return await fn();
+    } catch (error) {
+      if (i < retries - 1) {
+        await new Promise((resolve) => setTimeout(resolve, delay));
+      } else {
+        throw error;
+      }
+    }
+  }
+};
 
 (async () => {
   // Set up an Express server
@@ -28,14 +43,20 @@ const fs = require('fs');
     // Test for specific <li> content
     const expectedLink = '/?q=mn1';
     const expectedText = 'MN1: The Explanation of the Root';
-    const linkExists = await sutta.$eval('ul', (ul, expectedLink, expectedText) => {
-      const li = [...ul.querySelectorAll('li')].find(li =>
-        li.querySelector('a') &&
-        li.querySelector('a').getAttribute('href') === expectedLink &&
-        li.querySelector('a').textContent.includes(expectedText)
-      );
-      return !!li;
-    }, expectedLink, expectedText);
+    const linkExists = await sutta.$eval(
+      'ul',
+      (ul, expectedLink, expectedText) => {
+        const li = [...ul.querySelectorAll('li')].find(
+          (li) =>
+            li.querySelector('a') &&
+            li.querySelector('a').getAttribute('href') === expectedLink &&
+            li.querySelector('a').textContent.includes(expectedText)
+        );
+        return !!li;
+      },
+      expectedLink,
+      expectedText
+    );
 
     if (!linkExists) throw new Error(`No <li> found with the specific <a> tag content`);
   }
@@ -49,12 +70,12 @@ const fs = require('fs');
     if (!mn1Element) throw new Error('Element with ID "mn1" not found');
 
     // Test if the mn1 element contains <p> tags
-    const pTagsExist = await mn1Element.$$eval('p', pTags => pTags.length > 0);
+    const pTagsExist = await mn1Element.$$eval('p', (pTags) => pTags.length > 0);
     if (!pTagsExist) throw new Error('<p> tags under #mn1 not found');
 
     // Test for comments
-    const commentsExist = await mn1Element.$$eval('.comment-text', comments =>
-      comments.every(comment => comment.style.display === 'none')
+    const commentsExist = await mn1Element.$$eval('.comment-text', (comments) =>
+      comments.every((comment) => comment.style.display === 'none')
     );
     if (!commentsExist) throw new Error('Comments with class "comment-text" not found or not hidden');
   }
@@ -75,7 +96,7 @@ const fs = require('fs');
         console.log('Link not found');
       }
     } catch (error) {
-      throw new Error(`No <a> tag found with href="/?q=${inputCitation}"`)
+      throw new Error(`No <a> tag found with href="/?q=${inputCitation}"`);
     }
   }
 
@@ -95,12 +116,12 @@ const fs = require('fs');
         console.log('Link not found');
       }
     } catch (error) {
-      throw new Error(`No <a> tag found with href="/?q=${ExpectedCitation}"`)
+      throw new Error(`No <a> tag found with href="/?q=${ExpectedCitation}"`);
     }
   }
   async function testAllCacheFilesReachable(jsonFilePath) {
     const fileList = JSON.parse(fs.readFileSync(jsonFilePath));
-    const checkPromises = fileList.map(async filePath => {
+    const checkPromises = fileList.map(async (filePath) => {
       const page = await browser.newPage();
       const fullUrl = `http://localhost:${port}/${filePath}`;
 
@@ -108,7 +129,7 @@ const fs = require('fs');
         const response = await page.goto(fullUrl, {
           waitUntil: 'networkidle2',
           method: 'HEAD',
-          timeout: 10000 // set timeout to 10 seconds
+          timeout: 10000, // set timeout to 10 seconds
         });
 
         if (response.status() === 404) {
@@ -160,6 +181,59 @@ const fs = require('fs');
     }
   }
 
+  async function testBookmarkJsonUpload() {
+    await page.goto(`http://localhost:${port}/bookmarks.html`);
+
+    const jsonContent = {
+      bookmarks: {
+        unlabeled: ['mn9:11.4'],
+        similes: ['mn20:4.5-mn20:4.6'],
+      },
+      updatedAt: '2024-06-25T19:47:10.162Z',
+      detailsState: {
+        unlabeled: true,
+        similes: true,
+      },
+    };
+    // Create a temporary JSON file
+    const filePath = path.join(__dirname, 'tempBookmarks.json');
+    fs.writeFileSync(filePath, JSON.stringify(jsonContent));
+
+    // Upload the JSON file
+    const [fileChooser] = await Promise.all([
+      page.waitForFileChooser(),
+      page.click('#uploadButton'), // Click the upload button to trigger the file chooser
+    ]);
+    await fileChooser.accept([filePath]);
+
+    await retry(async () => {
+      // Check for relevant features in the HTML
+      const detailsElements = await page.$$('details');
+
+      for (const details of detailsElements) {
+        const summaryText = await details.$eval('summary.bookmark-text', (el) => el.textContent);
+        if (summaryText.includes('similes (1)')) {
+          const similesText = await details.$eval('.bookmark-container p', (el) => el.textContent);
+          if (
+            !similesText.includes('Suppose there was a woman or man who was young, youthful, and fond of adornments.')
+          ) {
+            throw new Error('Similes text not found or incorrect');
+          }
+
+          const similesLink = await details.$eval('.bookmark-container a', (el) => el.href);
+          if (!similesLink.includes('/?q=mn20#mn20:4.5-mn20:4.6')) {
+            throw new Error('Similes link not found or incorrect');
+          }
+
+          const similesCheckbox = await details.$eval('#checkbox-similes', (el) => el.checked);
+          if (!similesCheckbox) {
+            throw new Error('Similes checkbox not found or not checked');
+          }
+        }
+      }
+    });
+  }
+
   // Run tests
   try {
     await testHomePage();
@@ -169,6 +243,8 @@ const fs = require('fs');
     await testPaliTitleSearch('sabba', 'mn2');
     await testAllCacheFilesReachable('files_to_cache.json');
     await testBookmarkLabelCreation();
+    await testBookmarkJsonUpload();
+
     console.log('All tests passed successfully.');
   } catch (error) {
     console.error('Test failed: ', error);
