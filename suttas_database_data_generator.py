63fb542b9ce27a27a79df2b1cd07fb17b6d7c3d7
interested-in-dhamma <flynheijmans5@gmail.com>
2024-10-12 22:23:13 +0200

    re-order

--- suttas_database_data_generator.py
+++ suttas_database_data_generator.py
@@ -5,9 +5,9 @@ import json
 json_translation_directory = './suttas/translation_en'
 json_root_directory = './suttas/root'
 json_comments_directory = './suttas/comment'
+headings_file_path = './suttas/translation_en/headings.json'
 combined_json_path = './python-generated/suttas-database-data.json'
 suttas_count_js_path = './python-generated/suttas-count.js'
-headings_file_path = './suttas/translation_en/headings.json'
 
 def combine_translations(translation_directory, root_directory, comment_directory, headings):
     combined_data = {}
