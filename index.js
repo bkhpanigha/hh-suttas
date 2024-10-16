e82e904ca43086d82685a68245c8ee2cbbea0816
gachi <59866185+Reptilioo@users.noreply.github.com>
2024-10-16 14:23:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -541,8 +541,8 @@ epubInfoButton.addEventListener("click", function (event) {
 
 document.getElementById('downloadEpubButton').addEventListener('click', function() {
     const link = document.createElement('a');
-    link.href = 'suttas_epub/Sutta_Translations.epub';  // Chemin vers le fichier EPUB
-    link.download = 'Sutta_Translations.epub';    // Nom du fichier téléchargé
+    link.href = '/suttas_epub/Sutta_Translations.epub';  // Path to EPUB file
+    link.download = 'Sutta_Translations.epub';    // Downloaded file name
     link.click();
 });
 
