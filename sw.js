0c22bc58b686020bb0232d5d6ddf6be65cc21482
gachi <59866185+Reptilioo@users.noreply.github.com>
2024-10-26 15:30:18 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const version = 'v5'; // Update this version manually when filesToCache changes
+const version = 'v6'; // Update this version manually when filesToCache changes
 
 // static files to cache
 const filesToCache = [
