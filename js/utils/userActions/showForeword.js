94769c5996299c604ab2dba51c53dea2441b5e3d
gachi <59866185+Reptilioo@users.noreply.github.com>
2024-10-27 14:12:31 +0100

    Update showForeword.js

--- js/utils/userActions/showForeword.js
+++ js/utils/userActions/showForeword.js
@@ -1,7 +1,9 @@
 import { forewordText } from "../../rawTextContent/forewordText.js";
+import getDocumentAreas from "../getDocumentAreas.js";
 
 export function showForeword() 
 {
+    const { suttaArea } = getDocumentAreas();
     const forewordButton = document.getElementById('foreword-button');
     suttaArea.innerHTML = `<p>${forewordText}</p>`;
     forewordButton.style.display = 'none';
