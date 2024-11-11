a89c7a3eeea4b79457207a4a1cce7b65e84b6329
gachi <59866185+Reptilioo@users.noreply.github.com>
2024-11-11 17:42:33 +0100

    Update preventFlashing.js

--- js/utils/navigation/preventFlashing.js
+++ js/utils/navigation/preventFlashing.js
@@ -1,6 +1,4 @@
 export function preventFlashing(){
-	document.body.classList.add(localStorage.theme);
 	//always reveal default content AFTER loading -- prevents flashing.
 	document.getElementById('appbody').classList.remove('hidden');
-	document.documentElement.classList.remove(localStorage.theme);
 }
