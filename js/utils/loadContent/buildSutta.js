c76373c16879c87d8df24d38dc568a39dd73b1f9
gachi <59866185+Reptilioo@users.noreply.github.com>
2024-11-13 16:18:14 +0100

    Fix more flashing (#232)
    * Update buildSutta.js
    
    * Update index.js

--- js/utils/loadContent/buildSutta.js
+++ js/utils/loadContent/buildSutta.js
@@ -23,117 +23,130 @@ const getSuttaNavigation = (slug, availableSuttasJson) => {
   return `<div class="suttaNavigation">${previousSuttaButton}${nextSuttaButton}</div>`
 }
 
-export function buildSutta(slug, availableSuttasJson) 
-{
-    const footer = document.getElementById('footer');
-    const originalDisplay = footer.style.display;
-    footer.style.display = 'none';
+export function buildSutta(slug, availableSuttasJson) {
+    return new Promise((resolve, reject) => {
+        const footer = document.getElementById('footer');
+        const originalDisplay = footer.style.display;
+        // Hide footer to prevent flashing
+        footer.style.display = 'none';
+		
+        const {suttaArea} = getDocumentAreas();
+		
+        if (!slug) {
+            suttaArea.innerHTML = '<div style="display: flex; justify-content: center;"><span id="foreword-button" class="foreword-link">Foreword</span></div>';
+            resolve();
+            return;
+        }
 
-    const converter = new showdown.Converter();
-    const {suttaArea} = getDocumentAreas();
+        const converter = new showdown.Converter();
 
-    slug = slug.toLowerCase();
-    const sutta_details = availableSuttasJson[slug]
+        slug = slug.toLowerCase();
+        const sutta_details = availableSuttasJson[slug]
+        if(!sutta_details){
+			footer.style.display = originalDisplay;
+            resolve();
+            return;
+        }
+		
+        let translator = "Bhikkhu Anīgha";
+        let html = `<div class="button-area"><button id="hide-pali" class="hide-button">Toggle Pali</button></div>`;
+        const sutta_title = sutta_details['title'];
+        const acronym = sutta_details['id'];
 
-    let translator = "Bhikkhu Anīgha";
-    let html = `<div class="button-area"><button id="hide-pali" class="hide-button">Toggle Pali</button></div>`;
-    const sutta_title = sutta_details['title'];
-    const acronym = sutta_details['id'];
+        const rootResponse = fetch(sutta_details['root_path']).then(response => response.json());
+        const translationResponse = fetch(sutta_details['translation_path']).then(response => response.json());
+        const htmlResponse = fetch(sutta_details['html_path']).then(response => response.json());
+        const commentResponse = fetch(sutta_details['comment_path'])
+        .then(response => {
+            if (!response.ok) throw new Error(`Comment file not found for ${slug}`);
+            return response.json();
+        })
+        .catch(error => {
+            console.warn(error.message);
+            return; // Return an empty object if the comment file is not found
+        });
 
-    const rootResponse = fetch(sutta_details['root_path']).then(response => response.json());
-    const translationResponse = fetch(sutta_details['translation_path']).then(response => response.json());
-    const htmlResponse = fetch(sutta_details['html_path']).then(response => response.json());
-    const commentResponse = fetch(sutta_details['comment_path'])
-    .then(response => {
-      if (!response.ok) throw new Error(`Comment file not found for ${slug}`);
-      return response.json();
-    })
-    .catch(error => {
-      console.warn(error.message);
-      return {}; // Return an empty object if the comment file is not found
-    });
+        const authors = fetch(`authors.json`).then(response => response.json());
 
-    const authors = fetch(`authors.json`).then(response => response.json());
+        // Get root, translation and html jsons from folder
+        Promise.all([htmlResponse, rootResponse, translationResponse, commentResponse, authors])
+            .then(responses => {
+                const [html_text, root_text, translation_text, comment_text, authors_text] = responses;
+                const keys_order = Object.keys(html_text);
+                let commentCount = 1;
+                let commentsHtml = '';
 
-    // Get root, translation and html jsons from folder
-    Promise.all([htmlResponse, rootResponse, translationResponse, commentResponse, authors])
-        .then(responses => {
-        const [html_text, root_text, translation_text, comment_text, authors_text] = responses;
-        const keys_order = Object.keys(html_text);
-        let commentCount = 1;
-        let commentsHtml = '';
+                keys_order.forEach((segment) => {
+                    if (translation_text[segment] === undefined) translation_text[segment] = "";
 
-        keys_order.forEach((segment) => 
-        {
-            if (translation_text[segment] === undefined) translation_text[segment] = "";
+                    let [openHtml, closeHtml] = html_text[segment].split(/{}/);
 
-            let [openHtml, closeHtml] = html_text[segment].split(/{}/);
+                    if (window.addBreaks === true) {
+                        openHtml = openHtml.replace(/^<span class='verse-line'>/, "<br><span class='verse-line'>");
+                    }
 
-            if (window.addBreaks === true) 
-            {
-                openHtml = openHtml.replace(/^<span class='verse-line'>/, "<br><span class='verse-line'>");
-            }
+                    html +=
+                    `${openHtml}<span class="segment" id="${segment}">` +
+                    `<span class="pli-lang" lang="pi">${root_text[segment] || ''}</span>` +
+                    `<span class="eng-lang" lang="en">${translation_text[segment]}` +
+                    `${comment_text[segment] ? `<a href="${window.location.origin}?q=${slug}#comment${commentCount}" class="comment">[${commentCount}]</a>` : ''}` +
+                    `</span></span>${closeHtml}\n\n`;
+                
+                    if (comment_text[segment]) {
+                        if(commentCount == 1) commentsHtml += '<h3>Comments</h3>';
 
-            html +=
-            `${openHtml}<span class="segment" id="${segment}">` +
-            `<span class="pli-lang" lang="pi">${root_text[segment] || ''}</span>` +
-            `<span class="eng-lang" lang="en">${translation_text[segment]}` +
-            `${comment_text[segment] ? `<a href="${window.location.origin}?q=${slug}#comment${commentCount}" class="comment">[${commentCount}]</a>` : ''}` +
-            `</span></span>${closeHtml}\n\n`;
-        
-            if (comment_text[segment]) 
-            {
-                if(commentCount == 1) commentsHtml += '<h3>Comments</h3>';
+                        // Inside the comment HTML
+                        commentsHtml += `
+                        <p id="comment${commentCount}"><span>
+                        ${commentCount}: ${converter.makeHtml(comment_text[segment])
+                            .replace(/^<p>(.*)<\/p>$/, '$1')}
+                        <a href="${window.location.origin}?q=${slug}#${segment}~no-highlight" style="cursor: pointer; font-size: 14px;">&larr;</a>
+                        </span></p>
+                        `;
+            
+                        commentCount++;
+                    }
+                });
 
-                // Inside the comment HTML
-                commentsHtml += `
-                <p id="comment${commentCount}"><span>
-                ${commentCount}: ${converter.makeHtml(comment_text[segment])
-                    .replace(/^<p>(.*)<\/p>$/, '$1')}
-                <a href="${window.location.origin}?q=${slug}#${segment}~no-highlight" style="cursor: pointer; font-size: 14px;">&larr;</a>
-                </span></p>
-                `;
-    
-                commentCount++;
-          }
-      });
+                if (authors_text[slug]) translator = authors_text[slug];
+                const translatorByline = `<div class="byline"><p>Translated by ${translator}</p></div>`;
+                
+                const suttaNavigation = getSuttaNavigation(slug, availableSuttasJson);
 
-      if (authors_text[slug]) translator = authors_text[slug];
-      const translatorByline = `<div class="byline"><p>Translated by ${translator}</p></div>`;
-      
-      const suttaNavigation = getSuttaNavigation(slug, availableSuttasJson);
+                suttaArea.innerHTML = `<p class="sc-link"></p>` + html + translatorByline + suttaNavigation + commentsHtml;
+                
+                document.title = `${acronym} ${sutta_title}`;
 
-      suttaArea.innerHTML = `<p class="sc-link"></p>` + html + translatorByline + suttaNavigation + commentsHtml;
-      
-      document.title = `${acronym} ${sutta_title}`;
+                initializePaliToggle();
+                addNavbar();
 
-      initializePaliToggle();
-      addNavbar();
+                // remove download and info button
+                const cacheButton = document.getElementById('cacheButton');
+                const infoButton = document.getElementById('infoButton');
+                const downloadEpubButton = document.getElementById('downloadEpubButton');
+                const epubInfoButton = document.getElementById('epubInfoButton');
+                if (cacheButton) cacheButton.style.display = 'none';
+                if (infoButton) infoButton.style.display = 'none';
+                if (downloadEpubButton) downloadEpubButton.style.display = 'none';      
+                if (epubInfoButton) epubInfoButton.style.display = 'none';
+                
+                // highlight search term
+                checkSearchUrlParam();
+                
+                // scroll to the quote in the url if present
+                scrollToHash();
 
-      // remove download and info button
-      const cacheButton = document.getElementById('cacheButton');
-      const infoButton = document.getElementById('infoButton');
-      const downloadEpubButton = document.getElementById('downloadEpubButton');
-      const epubInfoButton = document.getElementById('epubInfoButton');
-      if (cacheButton) cacheButton.style.display = 'none';
-      if (infoButton) infoButton.style.display = 'none';
-      if (downloadEpubButton) downloadEpubButton.style.display = 'none';      
-      if (epubInfoButton) epubInfoButton.style.display = 'none';
+                // display footer again once everything is ready
+                footer.style.display = originalDisplay;
 
-      footer.style.display = originalDisplay;
-      
-	  // highlight search term
-	  checkSearchUrlParam();
-	  
-      // scroll to the quote in the url if present
-      scrollToHash();
-	  
-	  
-    })
-    .catch(error => {
-      console.log(error);
-      suttaArea.innerHTML = `<p>Sorry, "${decodeURIComponent(slug)}" is not a valid sutta citation.
-
-    <br><br>Note: Make sure the citation code is correct. Otherwise try finding the sutta from the home page.<br>`;
+                resolve();
+            })
+            .catch(error => {
+                console.log(error);
+                suttaArea.innerHTML = `<p>Sorry, "${decodeURIComponent(slug)}" is not a valid sutta citation.
+                <br><br>Note: Make sure the citation code is correct. Otherwise try finding the sutta from the home page.<br>`;
+                footer.style.display = originalDisplay;
+                reject(error);
+            });
     });
 }
