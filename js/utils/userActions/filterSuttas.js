24cb6407cdb5e518cc9ed7d8781b5e992308365c
gachi <59866185+Reptilioo@users.noreply.github.com>
2025-01-17 15:08:30 +0100

    Update filterSuttas.js

--- js/utils/userActions/filterSuttas.js
+++ js/utils/userActions/filterSuttas.js
@@ -1,15 +1,20 @@
+import { getFuseInstance } from "../misc/createFuseSearch.js";
+
 export function filterSuttas(pattern) {
-	if (!fuse) { pattern = "" }; // if Fuse isn't initialized, return empty array
-	
-	pattern = pattern.normalize('NFD').replace(/[\u0300-\u036f]/g, ''); // Convert pali letters in latin letters to match pali_title in available_suttas.json
-	pattern = pattern.replace(/\s+/g, ' ') // Removes multiple spaces
-	.replace(/(^|\s)/g, " '");
+  const fuse = getFuseInstance();
+  if (!fuse) {
+    pattern = ""
+  }; // if Fuse isn't initialized, return empty array
 
-	let results = fuse.search(pattern).reduce((acc, result) => {
-		acc[result.item.id] = result.item;
-		return acc;
-	}, {});
+  pattern = pattern.normalize('NFD').replace(/[\u0300-\u036f]/g, ''); // Convert pali letters in latin letters to match pali_title in available_suttas.json
+  pattern = pattern.replace(/\s+/g, ' ') // Removes multiple spaces
+    .replace(/(^|\s)/g, " '");
 
-	// join up the id with the titles to be displayed
-	return results;
+  let results = fuse.search(pattern).reduce((acc, result) => {
+    acc[result.item.id] = result.item;
+    return acc;
+  }, {});
+
+  // join up the id with the titles to be displayed
+  return results;
 }
