5b16960855ab5c22aa428fad464b29aae669c50d
gachi <59866185+Reptilioo@users.noreply.github.com>
2025-01-17 15:08:57 +0100

    Update and rename activateSearchBar.js to activateFilterBar.js

---
+++ js/utils/eventListeners/activateFilterBar.js
@@ -0,0 +1,23 @@
+import getDocumentAreas from "../getDocumentAreas.js";
+import { filterSuttas } from "../userActions/filterSuttas.js";
+
+export default function activateFilterBar() {
+  const { filterBar, suttaArea } = getDocumentAreas();
+  filterBar.focus();
+
+  filterBar.addEventListener("input", async (e) => {
+    const filterQuery = e.target.value.trim().toLowerCase();
+    suttaArea.innerHTML = "";
+
+    if (filterQuery) {
+      const filterResults = filterSuttas(filterQuery);
+      if (Object.keys(filterResults).length > 0) {
+        displaySuttas(filterResults, true);
+      } else {
+        suttaArea.innerHTML += "<h2 class=\"no-results\">No results found</h2>";
+      }
+    } else {
+      displaySuttas(availableSuttasJson);
+    }
+  });
+}
