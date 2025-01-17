164e5497f757795f23708c3045ce459a86dd2835
gachi <59866185+Reptilioo@users.noreply.github.com>
2025-01-17 16:31:56 +0100

    Update getDocumentAreas.js

--- js/utils/getDocumentAreas.js
+++ js/utils/getDocumentAreas.js
@@ -4,14 +4,14 @@ export default function getDocumentAreas()
 {
     let suttaArea = null;
     let whatsNewArea = null;
-    let form = null;
+    let filterForm = null;
     if (!window.location.href.endsWith("/bookmarks.html") 
 	&& !window.location.href.endsWith("/glossary.html") 
 	&& !window.location.href.endsWith("/comments.html")
 	&& !window.location.href.endsWith("/advanced-search.html")){
         suttaArea = document.getElementById("sutta");
         whatsNewArea = document.getElementById('whats-new');
-        form = document.getElementById('form');
+        filterForm = document.getElementById('filter-form');
     }
     const homeButton = document.getElementById("home-button");
     const themeButton = document.getElementById("theme-button");
@@ -30,7 +30,7 @@ export default function getDocumentAreas()
         previous, 
         next, 
         filterBar,
-        form,
+        filterForm,
         infoButton,
     };
 }
