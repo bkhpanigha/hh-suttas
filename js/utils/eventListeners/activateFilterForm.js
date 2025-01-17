7185c3f3c9c69fb86b44798334d4449b3c12a121
gachi <59866185+Reptilioo@users.noreply.github.com>
2025-01-17 16:33:29 +0100

    Update and rename activateForm.js to activateFilterForm.js

--- js/utils/eventListeners/activateFilterForm.js
+++ js/utils/eventListeners/activateFilterForm.js
@@ -2,11 +2,11 @@ import getDocumentAreas from "../getDocumentAreas.js";
 import { buildSutta } from "../loadContent/buildSutta.js";
 
 
-export default function activateForm()
+export default function activateFilterForm()
 {
-    const {form} = getDocumentAreas();
+    const { filterForm } = getDocumentAreas();
 
-    form.addEventListener("submit", e => 
+    filterForm.addEventListener("submit", e => 
     {
         e.preventDefault();
         const searchValue = searchBar.value.trim().replace(/\s/g, "");
