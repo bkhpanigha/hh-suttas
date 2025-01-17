e6fe442119b3c4f2ebce9bda9106701c7d02650b
gachi <59866185+Reptilioo@users.noreply.github.com>
2025-01-17 14:29:43 +0100

    Create filterSuttas.js

---
+++ js/utils/userActions/filterSuttas.js
@@ -0,0 +1,15 @@
+export function filterSuttas(pattern) {
+	if (!fuse) { pattern = "" }; // if Fuse isn't initialized, return empty array
+	
+	pattern = pattern.normalize('NFD').replace(/[\u0300-\u036f]/g, ''); // Convert pali letters in latin letters to match pali_title in available_suttas.json
+	pattern = pattern.replace(/\s+/g, ' ') // Removes multiple spaces
+	.replace(/(^|\s)/g, " '");
+
+	let results = fuse.search(pattern).reduce((acc, result) => {
+		acc[result.item.id] = result.item;
+		return acc;
+	}, {});
+
+	// join up the id with the titles to be displayed
+	return results;
+}
