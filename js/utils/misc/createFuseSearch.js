1af3dfe44e5fb1c912f68b1df9d98a9dab9188f3
gachi <59866185+Reptilioo@users.noreply.github.com>
2025-01-17 14:31:32 +0100

    Create createFuseSearch.js

---
+++ js/utils/misc/createFuseSearch.js
@@ -0,0 +1,21 @@
+export async function createFuseSearch(availableSuttasJson) {
+	//Combine all values in a single field so user can do search on multiple fields
+	let searchDict = Object.entries(availableSuttasJson).map(([sutta_id, sutta_details]) => {
+		// Declare search fields here
+		let sutta_details_without_fp = (({ id, title, pali_title, heading}) => ({ id, title, pali_title, heading}))(sutta_details);
+		
+		sutta_details_without_fp['citation'] = sutta_id;
+		// Get every element's values and combine them with a white space
+		const combination = Object.values(sutta_details_without_fp).join(' ')
+		  .normalize('NFD').replace(/[\u0300-\u036f]/g, ''); //pali normalized in latin for search to work on headings containing pali
+		
+		// Return new object with "combination" key added
+		return {
+		  ...sutta_details_without_fp,
+		  combination: combination
+		};
+	});
+	
+	fuse = new Fuse(searchDict, fuseOptions);
+	return fuse
+}
