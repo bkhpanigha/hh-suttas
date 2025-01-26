504e6825e153a51391a9cf2d9350d0a40879bc7f
gachi <59866185+Reptilioo@users.noreply.github.com>
2025-01-26 12:02:07 +0100

    Update and rename activateViewForewordAndGoBackButtons.js to activateGoBackButtons.js

--- js/utils/eventListeners/activateGoBackButtons.js
+++ js/utils/eventListeners/activateGoBackButtons.js
@@ -1,17 +1,10 @@
 import { displaySuttas } from "../contentSections/displaySuttas.js";
 import { goBack } from "../navigation/goBack.js";
-import { showForeword } from "../userActions/showForeword.js";
-
 
 export default function activateViewForewordAndGoBackButtons(availableSuttasJson)
 {
     document.addEventListener('click', function (event) 
     {
-        // Check if the clicked element is the foreword button
-        if (event.target && event.target.id === 'foreword-button') {
-            showForeword(); // Call the function to show the foreword
-            displaySuttas(availableSuttasJson);
-        }
         // Add a click event listener to the span element
         if (event.target && event.target.id === 'back-arrow') {
             goBack();
