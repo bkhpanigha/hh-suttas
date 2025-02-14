0fd31661370788565173250f85960b64273be86f
gachi <59866185+Reptilioo@users.noreply.github.com>
2025-02-14 23:43:37 +0100

    Update initializePaliToggle.js
    Prevents js error on page without `suttaArea` element

--- js/utils/eventListeners/initializePaliToggle.js
+++ js/utils/eventListeners/initializePaliToggle.js
@@ -7,38 +7,47 @@ export function initializePaliToggle() {
 
     if (localStorage.paliToggle !== "show") {
         localStorage.paliToggle = "hide";
-        suttaArea.classList.add("hide-pali");
+		if (suttaArea)
+			suttaArea.classList.add("hide-pali");
     }
 
     const updatePaliSetting = () => {
         isTogglingPali = true; // Temporarily disable the scroll event
 
-        const englishElements = Array.from(suttaArea.querySelectorAll(".eng-lang"));
-        const firstVisibleEnglishElement = englishElements.find(el => {
-            const rect = el.getBoundingClientRect();
-            return rect.bottom >= 0 && rect.top <= window.innerHeight;
-        });
-
         const togglePali = () => {
             if (localStorage.paliToggle === "show") {
-                suttaArea.classList.add("hide-pali");
+				if (suttaArea){
+					suttaArea.classList.add("hide-pali");
+					document.body.classList.remove("side-by-side");
+				}
                 localStorage.paliToggle = "hide";
-                document.body.classList.remove("side-by-side");
                 localStorage.sideBySide = "false";
             } else {
-                suttaArea.classList.remove("hide-pali");
+				if (suttaArea)
+					suttaArea.classList.remove("hide-pali");
                 localStorage.paliToggle = "show";
             }
         };
 
-        if (firstVisibleEnglishElement) {
-            const prevOffset = firstVisibleEnglishElement.getBoundingClientRect().top;
-            togglePali();
-            const newOffset = firstVisibleEnglishElement.getBoundingClientRect().top;
-            window.scrollBy(0, newOffset - prevOffset);
-        } else {
-            togglePali();
-        }
+        const englishElements = suttaArea?.querySelectorAll(".eng-lang");
+
+		if (englishElements && englishElements.length > 0) {
+			const firstVisibleEnglishElement = Array.from(englishElements).find(el => {
+				const rect = el.getBoundingClientRect();
+				return rect.bottom >= 0 && rect.top <= window.innerHeight;
+			});
+
+			if (firstVisibleEnglishElement) {
+				const prevOffset = firstVisibleEnglishElement.getBoundingClientRect().top;
+				togglePali();
+				const newOffset = firstVisibleEnglishElement.getBoundingClientRect().top;
+				window.scrollBy(0, newOffset - prevOffset);
+			} else {
+				togglePali();
+			}
+		} else {
+			togglePali();
+		}
 
         setTimeout(() => {
             isTogglingPali = false; // Re-enable the scroll event after a short delay
