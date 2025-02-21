8693ba9a33e66e65d30e64f3f7a825574c813e0f
gachi <59866185+Reptilioo@users.noreply.github.com>
2025-02-21 15:41:50 +0100

    Update search-panel.js

--- search-panel.js
+++ search-panel.js
@@ -1,4 +1,3 @@
-import { initializeSearchEvents } from './js/utils/eventListeners/initializeSearchEvents.js';
 import { searchSuttasWithStop } from './js/utils/contentSections/searchSuttasWithStop.js';
 import { startSearch } from './js/utils/userActions/startSearch.js';
 import { searchState } from './js/utils/userActions/searchState.js';
@@ -9,15 +8,3 @@ import { preventFlashing } from './js/utils/navigation/preventFlashing.js';
 window.startSearch = startSearch;
 window.searchSuttasWithStop = searchSuttasWithStop;
 window.searchState = searchState;
-
-// Initialize search events once the DOM is loaded
-document.addEventListener('DOMContentLoaded', () => {
-	try {
-		initializeSearchEvents();
-	} catch (error) {
-		console.error('[ERROR] Something went wrong:', error);
-	}
-	finally {
-		preventFlashing();
-	}
-});
