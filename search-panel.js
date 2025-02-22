b5ee7490078039b332a535f6c623ce189282b539
gachi <59866185+Reptilioo@users.noreply.github.com>
2025-02-22 13:19:33 +0100

    Remove unused

--- search-panel.js
+++ search-panel.js
@@ -1,7 +1,6 @@
 import { searchSuttasWithStop } from './js/utils/contentSections/searchSuttasWithStop.js';
 import { startSearch } from './js/utils/userActions/startSearch.js';
 import { searchState } from './js/utils/userActions/searchState.js';
-import { preventFlashing } from './js/utils/navigation/preventFlashing.js';
 
 // Make main functions globally available
 // This is necessary as they are referenced in the HTML
