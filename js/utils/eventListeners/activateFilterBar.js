35a2f9310ce2b99111df01c176f17af0efaaf5ed
interested-in-dhamma <flynheijmans5@gmail.com>
2025-06-16 19:36:02 +0200

    Feature/304/suttas-addition-history-view (#306)
    * add suttas history view + refactoring.
    
    * refactor
    
    * fix dark mode styling support button
    
    * add dark mode colours for tab switch

--- js/utils/eventListeners/activateFilterBar.js
+++ js/utils/eventListeners/activateFilterBar.js
@@ -1,9 +1,9 @@
 import getDocumentAreas from "../getDocumentAreas.js";
 import { filterSuttas } from "../userActions/filterSuttas.js";
-import { displaySuttas } from "../contentSections/displaySuttas.js";
+import { displaySuttasLibrary, displaySuttasHistory } from "../contentSections/displaySuttas.js";
 
 export default function activateFilterBar(availableSuttasJson) {
-  const { filterBar, suttaArea } = getDocumentAreas();
+  const { filterBar, filterForm, suttasTabSwitcher, suttaArea } = getDocumentAreas();
 
   filterBar.addEventListener("input", async (e) => {
     const filterQuery = e.target.value.trim().toLowerCase();
@@ -12,12 +12,24 @@ export default function activateFilterBar(availableSuttasJson) {
     if (filterQuery) {
       const filterResults = filterSuttas(filterQuery);
       if (Object.keys(filterResults).length > 0) {
-        displaySuttas(filterResults, true);
+        displaySuttasLibrary(filterResults, true);
       } else {
         suttaArea.innerHTML += "<h2 class=\"no-results\">No results found</h2>";
       }
     } else {
-      displaySuttas(availableSuttasJson);
+      displaySuttasLibrary(availableSuttasJson);
     }
   });
+
+  suttasTabSwitcher.addEventListener("change", (e) => {
+    if (e.target.checked) {
+      filterForm.classList.add("hide");
+      filterBar.value = "";
+      displaySuttasHistory(availableSuttasJson);
+      return;
+    }
+
+    filterForm.classList.remove("hide");
+    displaySuttasLibrary(availableSuttasJson);
+  })
 }
