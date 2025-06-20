b50f67c928200071fcfb5104490aa9ed8afeebaf
interested-in-dhamma <flynheijmans5@gmail.com>
2025-06-20 18:06:55 +0200

    add more shortcuts + shortcuts modal for info

--- js/utils/eventListeners/activateThemeButton.js
+++ js/utils/eventListeners/activateThemeButton.js
@@ -1,14 +1,15 @@
 import getDocumentAreas from "../getDocumentAreas.js";
 import { toggleTheme } from "../misc/toggleTheme.js";
 
+export const updateTheme = () => {
+    const currentThemeIsDark = localStorage.theme === "dark";
+    toggleTheme(!currentThemeIsDark);
+    return !currentThemeIsDark;
+};
+
 export function activateThemeButton()
 {
     const { themeButton, darkModeToggle } = getDocumentAreas();
-
-    const updateTheme = () => {
-        const currentThemeIsDark = localStorage.theme === "dark";
-        toggleTheme(!currentThemeIsDark);
-    };
 
     themeButton?.addEventListener("click", updateTheme);
     darkModeToggle?.addEventListener("click", updateTheme);
--- js/utils/eventListeners/initializePaliToggle.js
+++ js/utils/eventListeners/initializePaliToggle.js
@@ -2,88 +2,87 @@ import getDocumentAreas from "../getDocumentAreas.js";
 
 let isTogglingPali = false; // Global boolean to prevent scroll event issues
 
-export function initializePaliToggle() {
-  const { suttaArea, hidePaliButton, paliToggle, sideBySideLabel, sideBySideToggle } = getDocumentAreas();
+const { suttaArea, hidePaliButton, paliToggle, sideBySideLabel, sideBySideToggle } = getDocumentAreas();
 
-	const setSideBySideToggleDisabled = (disable) => {
-		if (!sideBySideLabel || !sideBySideToggle) return;
+const setSideBySideToggleDisabled = (disable) => {
+  if (!sideBySideLabel || !sideBySideToggle) return;
 
-		if (disable) {
-			sideBySideLabel.classList.add("disabled");
-			sideBySideToggle.disabled = true;
-			return;
-		}
-
-		sideBySideLabel.classList.remove("disabled");
-		sideBySideToggle.disabled = false;
-	}
-
-  if (localStorage.paliToggle !== "show") {
-    localStorage.paliToggle = "hide";
-    if (suttaArea) {
-			suttaArea.classList.add("hide-pali");
-		}
-
-		setSideBySideToggleDisabled(true);
+  if (disable) {
+    sideBySideLabel.classList.add("disabled");
+    sideBySideToggle.disabled = true;
+    return;
   }
 
-  const updatePaliSetting = () => {
-    isTogglingPali = true; // Temporarily disable the scroll event
+  sideBySideLabel.classList.remove("disabled");
+  sideBySideToggle.disabled = false;
+}
 
-    const togglePali = () => {
-			const isPaliOn = localStorage.paliToggle === "show";
+if (localStorage.paliToggle !== "show") {
+  localStorage.paliToggle = "hide";
+  if (suttaArea) {
+    suttaArea.classList.add("hide-pali");
+  }
 
-			setSideBySideToggleDisabled(isPaliOn);
+  setSideBySideToggleDisabled(true);
+}
 
-      if (isPaliOn) {
-        if (suttaArea) {
-          suttaArea.classList.add("hide-pali");
-          document.body.classList.remove("side-by-side");
-        }
+export const updatePaliSetting = () => {
+  isTogglingPali = true; // Temporarily disable the scroll event
 
-        localStorage.paliToggle = "hide";
-        // localStorage.sideBySide = "false";
-      } else {
-        if (suttaArea) {
-					suttaArea.classList.remove("hide-pali");
-				}
-        localStorage.paliToggle = "show";
+  const togglePali = () => {
+    const isPaliOn = localStorage.paliToggle === "show";
 
-				if (localStorage.sideBySide === "true") {
-					document.body.classList.add("side-by-side");
-				}
+    setSideBySideToggleDisabled(isPaliOn);
+
+    if (isPaliOn) {
+      if (suttaArea) {
+        suttaArea.classList.add("hide-pali");
+        document.body.classList.remove("side-by-side");
       }
-    };
 
-    const englishElements = suttaArea?.querySelectorAll(".eng-lang");
-
-    if (englishElements && englishElements.length > 0) {
-      const firstVisibleEnglishElement = Array.from(englishElements).find(
-        (el) => {
-          const rect = el.getBoundingClientRect();
-          return rect.bottom >= 0 && rect.top <= window.innerHeight;
-        }
-      );
-
-      if (firstVisibleEnglishElement) {
-        const prevOffset =
-          firstVisibleEnglishElement.getBoundingClientRect().top;
-        togglePali();
-        const newOffset =
-          firstVisibleEnglishElement.getBoundingClientRect().top;
-        window.scrollBy(0, newOffset - prevOffset);
-      } else {
-        togglePali();
+      localStorage.paliToggle = "hide";
+    } else {
+      if (suttaArea) {
+        suttaArea.classList.remove("hide-pali");
       }
+      localStorage.paliToggle = "show";
+
+      if (localStorage.sideBySide === "true") {
+        document.body.classList.add("side-by-side");
+      }
+    }
+  };
+
+  const englishElements = suttaArea?.querySelectorAll(".eng-lang");
+
+  if (englishElements && englishElements.length > 0) {
+    const firstVisibleEnglishElement = Array.from(englishElements).find(
+      (el) => {
+        const rect = el.getBoundingClientRect();
+        return rect.bottom >= 0 && rect.top <= window.innerHeight;
+      }
+    );
+
+    if (firstVisibleEnglishElement) {
+      const prevOffset =
+        firstVisibleEnglishElement.getBoundingClientRect().top;
+      togglePali();
+      const newOffset =
+        firstVisibleEnglishElement.getBoundingClientRect().top;
+      window.scrollBy(0, newOffset - prevOffset);
     } else {
       togglePali();
     }
+  } else {
+    togglePali();
+  }
 
-    setTimeout(() => {
-      isTogglingPali = false; // Re-enable the scroll event after a short delay
-    }, 100);
-  };
+  setTimeout(() => {
+    isTogglingPali = false; // Re-enable the scroll event after a short delay
+  }, 100);
+}
 
+export function initializePaliToggle() {
   hidePaliButton?.addEventListener("click", updatePaliSetting);
   paliToggle?.addEventListener("click", updatePaliSetting);
 }
