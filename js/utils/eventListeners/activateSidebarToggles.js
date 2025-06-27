bb65cccefb1646e002e43e995b4a511f683ae87b
Bhikkhu Anīgha <bkhpanigha@gmail.com>
2025-06-27 17:18:02 +0200

    widen sutta text on sidebar close

--- js/utils/eventListeners/activateSidebarToggles.js
+++ js/utils/eventListeners/activateSidebarToggles.js
@@ -4,17 +4,23 @@ export default function activateSidebarToggles() {
   const { leftSidebar, leftSidebarToggle, rightSidebar, rightSidebarToggle } = getDocumentAreas();
   if (!leftSidebar || !leftSidebarToggle || !rightSidebar || !rightSidebarToggle) return;
 
-  const activateSidebarToggle = (sidebar, toggle, storageKey) => {
-    const isHidden = localStorage[storageKey] === "true";
+  const activateSidebarToggle = (sidebar, toggle, storageKey, bodyClass) => {
+    // Initial state application
+    let isHidden = localStorage[storageKey] === "true";
     sidebar.classList.toggle("collapsed", isHidden);
+    document.body.classList.toggle(bodyClass, isHidden);
 
+    // Event listener for toggling
     toggle.addEventListener("click", () => {
-      const currentlyHidden = localStorage[storageKey] === "true";
-      sidebar.classList.toggle("collapsed", !currentlyHidden);
-      localStorage[storageKey] = !currentlyHidden;
+      isHidden = localStorage[storageKey] === "true"; // Get current state before toggle
+      const newHiddenState = !isHidden;
+      
+      sidebar.classList.toggle("collapsed", newHiddenState);
+      document.body.classList.toggle(bodyClass, newHiddenState);
+      localStorage[storageKey] = newHiddenState;
     });
   };
 
-  activateSidebarToggle(leftSidebar, leftSidebarToggle, "isLeftSidebarHidden");
-  activateSidebarToggle(rightSidebar, rightSidebarToggle, "isRightSidebarHidden");
+  activateSidebarToggle(leftSidebar, leftSidebarToggle, "isLeftSidebarHidden", "left-sidebar-is-collapsed");
+  activateSidebarToggle(rightSidebar, rightSidebarToggle, "isRightSidebarHidden", "right-sidebar-is-collapsed");
 }
