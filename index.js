480055528024d4d89de610d90da37d393f8b481f
interested-in-dhamma <flynheijmans5@gmail.com>
2025-09-07 15:51:38 +0200

    Feature/small behavior fix for quiet shortcut (#315)
    * update behavior for quiet mode to be same as clicking toggle
    
    * include max-width in the transition for smoothness
    
    * remove handleSidebarToggles

--- index.js
+++ index.js
@@ -43,7 +43,6 @@ import loadHomeControls from "./js/utils/contentSections/loadHomeControls.js";
 import { displaySuttasLibrary } from "./js/utils/contentSections/displaySuttas.js";
 import { activateEventListeners } from "./js/utils/loadContent/activateEventListeners.js";
 import { fetchAvailableSuttas } from "./js/utils/loadContent/fetchAvailableSuttas.js";
-import { handleSidebarToggles } from "./js/utils/misc/handleSidebarToggles.js";
 import { toggleTheme } from "./js/utils/misc/toggleTheme.js";
 import { buildSutta } from "./js/utils/loadContent/buildSutta.js";
 import updateSuttaDatabase from './js/database/updateSuttaDatabase.js';
@@ -86,7 +85,6 @@ document.addEventListener('DOMContentLoaded', async function() {
       // Additional setup steps
       updateSuttaDatabase();
       activateEventListeners(availableSuttasJson);
-      handleSidebarToggles();
       toggleTheme(initialThemeSetting);
       
       // Now we can reveal the content
